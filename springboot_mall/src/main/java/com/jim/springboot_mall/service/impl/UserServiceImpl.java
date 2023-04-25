b9e07d52f7cb83a8224beed6e5603c505728ab2a
zx412563 <zxchuahua2417@gmail.com>
2023-04-26 03:11:09 +0800

    使用MD5 加密密碼

--- springboot_mall/src/main/java/com/jim/springboot_mall/service/impl/UserServiceImpl.java
+++ springboot_mall/src/main/java/com/jim/springboot_mall/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.ResultSetSupportingSqlParameter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -26,12 +27,17 @@ public class UserServiceImpl implements UserService {
 
         //查詢 email 是否重複
         if (user != null) {
-            log.warn("該email {} 已被註冊",userRegisterRequest.getEmail());
+            log.warn("該email {} 已被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        } else {
-            //創建帳號
-            return userDao.createUser(userRegisterRequest);
         }
+        // 使用MD5 生成密碼雜湊值
+        String hashPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashPassword);
+
+
+        //創建帳號
+        return userDao.createUser(userRegisterRequest);
+
     }
 
     @Override
@@ -43,11 +49,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+        //檢查User是否存在
         if (user == null) {
             log.warn("該email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        //使用MD5生成密碼雜湊值
+        String hashPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+
+        //比較密碼
+        if (user.getPassword().equals(hashPassword)) {
             return user;
         } else {
             log.warn("你輸入的密碼 {} 不正確", userLoginRequest.getPassword());
